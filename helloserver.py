b991d7ff623aa2665186c1be05c49204e2d4a582
Eduardo Moreno <emoreno@uchicago.edu>
2015-09-28 11:33:35 -0700

    updates server for second route

--- helloserver.py
+++ helloserver.py
@@ -9,17 +9,11 @@ import os
 import sys
 import string
 import json
+import heapq
 import graphlab as gl
 from graphlab import SArray
 # from flask import url_for, redirect
 
-
-
-def init():
-	m = gl.load_model('topic_model_150_stopworded')
-	jtopics = m.get_topics()
-	return str(jtopics)
-	# jtopics = topics.export_json()
 
 def createSArray(arr):
 	return SArray(arr)
@@ -28,8 +22,19 @@ def createSArray(arr):
 def hello():
     return "Hello World!"
 
-@app.route(u'/new', methods=[u'POST'])
+@app.route(u'/location', methods=[u'POST'])
 def topify_doc():
+	arrTopicId =[{'id':182,'category':'buffet','score':0}, {'id':3,'category':'pizza','score':0}, 
+	{'id':148,'category':'Mexican','score':0}, {'id':243,'category':'bar','score':0}, {'id':154,'category':'lunch','score':0}, {'id':326,'category':'sushi','score':0}, {'id':52,'category':'ice cream','score':0}, {'id':303,'category':'cafe','score':0}, 
+	{'id':202,'category':'Vietnamese','score':0}, {'id':249,'category':'Chinese','score':0}, {'id':150,'category':'Fried Chicken','score':0}, {'id':64,'category':'breakfast','score':0}, {'id':245,'category':'burger','score':0}, 
+	{'id':163,'category':'Thai','score':0}, {'id':156,'category':'seafood','score':0}, {'id':26,'category':'sandwich','score':0}, {'id':240,'category':'Greek / Gyro','score':0}, {'id':151,'category':'donuts','score':0}, {'id':187,'category':'cheap','score':0}, 
+	{'id':159,'category':'Good Food','score':0}, {'id':167,'category':'Good Service','score':0},
+	{'id':190,'category':'Bad Service','score':0}, {'id':176,'category':'Indian','score':0}, {'id':179,'category':'Good Atmosphere','score':0}, {'id':256,'category':'Bad Atmosphere','score':0}, {'id':42,'category':'Hair Salon','score':0}, 
+	{'id':155,'category':'Hotel','score':0}, {'id':18,'category':'Doctor','score':0}, {'id':152,'category':'Yoga','score':0}, {'id':198,'category':'Ramen','score':0}, {'id':252,'category':'Movies','score':0}, 
+	{'id':247,'category':'Venue','score':0}, {'id':242,'category':'Salad','score':0}, {'id':233,'category':'Dog Friendly','score':0}, {'id':238,'category':'Clothing','score':0}, {'id':315,'category':'Happy Hour','score':0}, 
+	{'id':327,'category':'Dance / Club','score':0}, {'id':340,'category':'Mechanic','score':0}, {'id':61,'category':'Bagel / Bakery','score':0}, {'id':66,'category':'Italian','score':0}, {'id':72,'category':'Bbq','score':0}, 
+	{'id':82,'category':'Hawaiian','score':0}, {'id':86,'category':'Nail Salon','score':0}, {'id':108,'category':'Beer','score':0}]
+
 	# ipdb.set_trace()
 	print request.values
 	doc = json.loads(request.form.get('data')) #needs to be array of string
@@ -38,18 +43,80 @@ def topify_doc():
 	doc = doc.dict_trim_by_keys(gl.text_analytics.stopwords(), True)
 	doc = gl.SFrame(doc)
 	
-	pred = m.predict(doc) #alternatively, m.predict(doc, output_type='probability')
-	
-	result = m.get_topics(list(pred))
+	prob = m.predict(doc, output_type='probability') #alternatively, m.predict(doc, output_type='probability')
 
-	# result = result.pack_columns()
+	# new addition
 
-	unstacked = result.unstack(['word', 'score'], 'ws')
-	unstacked2 = unstacked.unstack(['topic', 'ws'], 'total')
+	lis = []
+
+	for topic in arrTopicId:
+		topic['score'] = prob[0][topic['id']]
+		slis = []
+		slis.append(topic['category'])
+		slis.append(topic['score'])
+		lis.append(slis)
+
+	resultTopics = heapq.nlargest(5, lis, key=lambda x: x[1])
 	
-	return jsonify(unstacked2['total'][0])
+	return jsonify(resultTopics)
+
+@app.route(u'/reviews', methods=[u'POST'])
+def topify_doc2():
+
+	# ipdb.set_trace()
+	print request.values
+	doc = json.loads(request.form.get('data')) #needs to be array of string
+	doc = gl.SFrame(doc)
+	doc = gl.text_analytics.count_words(doc['X1'])
+	doc = doc.dict_trim_by_keys(gl.text_analytics.stopwords(), True)
+	doc = gl.SFrame(doc)
+	
+	prob = m.predict(doc, output_type='probability')
+
+	# new addition
+
+	resultTopics = []
+
+	for scoreSet in prob:
+
+		arrTopicId =[{'id':182,'category':'buffet','score':0}, {'id':3,'category':'pizza','score':0}, 
+		{'id':148,'category':'Mexican','score':0}, {'id':243,'category':'bar','score':0}, {'id':154,'category':'lunch','score':0}, {'id':326,'category':'sushi','score':0}, {'id':52,'category':'ice cream','score':0}, {'id':303,'category':'cafe','score':0}, 
+		{'id':202,'category':'Vietnamese','score':0}, {'id':249,'category':'Chinese','score':0}, {'id':150,'category':'Fried Chicken','score':0}, {'id':64,'category':'breakfast','score':0}, {'id':245,'category':'burger','score':0}, 
+		{'id':163,'category':'Thai','score':0}, {'id':156,'category':'seafood','score':0}, {'id':26,'category':'sandwich','score':0}, {'id':240,'category':'Greek / Gyro','score':0}, {'id':151,'category':'donuts','score':0}, {'id':187,'category':'cheap','score':0}, 
+		{'id':159,'category':'Good Food','score':0}, {'id':167,'category':'Good Service','score':0},
+		{'id':190,'category':'Bad Service','score':0}, {'id':176,'category':'Indian','score':0}, {'id':179,'category':'Good Atmosphere','score':0}, {'id':256,'category':'Bad Atmosphere','score':0}, {'id':42,'category':'Hair Salon','score':0}, 
+		{'id':155,'category':'Hotel','score':0}, {'id':18,'category':'Doctor','score':0}, {'id':152,'category':'Yoga','score':0}, {'id':198,'category':'Ramen','score':0}, {'id':252,'category':'Movies','score':0}, 
+		{'id':247,'category':'Venue','score':0}, {'id':242,'category':'Salad','score':0}, {'id':233,'category':'Dog Friendly','score':0}, {'id':238,'category':'Clothing','score':0}, {'id':315,'category':'Happy Hour','score':0}, 
+		{'id':327,'category':'Dance / Club','score':0}, {'id':340,'category':'Mechanic','score':0}, {'id':61,'category':'Bagel / Bakery','score':0}, {'id':66,'category':'Italian','score':0}, {'id':72,'category':'Bbq','score':0}, 
+		{'id':82,'category':'Hawaiian','score':0}, {'id':86,'category':'Nail Salon','score':0}, {'id':108,'category':'Beer','score':0}]
+
+		lis = []
+
+		for topic in arrTopicId:
+			topic['score'] = scoreSet[topic['id']]
+			slis = []
+			slis.append(topic['category'])
+			slis.append(topic['score'])
+			lis.append(slis)
+
+		resultTopics.append(heapq.nlargest(5, lis, key=lambda x: x[1]))
+
+	# ipdb.set_trace()
+
+	returnDict = {"reviewTopics": resultTopics}
+	
+	return jsonify(returnDict)
+	
+    # old working version
+
+    # result = m.get_topics(list(pred))
+
+	# unstacked = result.unstack(['word', 'score'], 'ws')
+	# unstacked2 = unstacked.unstack(['topic', 'ws'], 'total')
+	
+	# return jsonify(unstacked2['total'][0])
 
 if __name__ == "__main__":
-    m = gl.load_model('topic_model_150_stopworded')
+    m = gl.load_model('topic_model_350_stopworded')
     app.run()
     
